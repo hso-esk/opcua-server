ffce91a5ec2d196ea7928fec7b94b2594a0874d7
Manuel Schappacher <manuel.schappacher@hs-offenburg.de>
2017-03-21 08:01:51 +0100

    fixed CMAKE defines

--- opcua-build.sh
+++ opcua-build.sh
@@ -6,5 +6,6 @@ mkdir -p asneg/build
 
 # create makefiles from CMake
 cd asneg/build
-cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKEBUILDTYPE=Debug -D CMAKEINSTALLPREFIX=./ ../src -DCMAKECCOMPILER=gcc-4.9 -DCMAKECXXCOMPILER=g++-4.9 -DCMAKERANLIB=gcc-ranlib-4.9 -DCMAKEAR=gcc-ar-4.9
+#cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=./ ../src -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_INSTALL_PREFIX=./ ../src -DCMAKE_C_COMPILER=gcc-4.9 -DCMAKE_CXX_COMPILER=g++-4.9
 make

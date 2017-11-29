bfd62a86816d14c501cdb8574d19a93248330ad8
Kofi Atta <kofi.nsiah@hahn-schickard.de>
2017-11-29 15:05:26 +0000

    added license information to ipso submodule

--- niki/ipso/OpcUaLWM2MLib.cpp
+++ niki/ipso/OpcUaLWM2MLib.cpp
@@ -1,9 +1,31 @@
 /*
- * OpcUaIPSOLib.cpp
- *
- *  Created on: 24 Feb 2017
- *      Author: osboxes
+ * --- License -------------------------------------------------------------- *
  */
+
+/*
+ * Copyright 2017 NIKI 4.0 project team
+ *
+ * NIKI 4.0 was financed by the Baden-Württemberg Stiftung gGmbH (www.bwstiftung.de).
+ * Project partners are FZI Forschungszentrum Informatik am Karlsruher
+ * Institut für Technologie (www.fzi.de), Hahn-Schickard-Gesellschaft
+ * für angewandte Forschung e.V. (www.hahn-schickard.de) and
+ * Hochschule Offenburg (www.hs-offenburg.de).
+ * This file was developed by [Kofi Atta Nsiah], [Hahn-Schickard-Gesellschaft
+ * für angewandte Forschung e.V.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 
 #include "OpcUaLWM2MLib.h"
 #include "OpcUaStackCore/Base/Log.h"
@@ -1379,7 +1401,6 @@ OpcUaDataValue::SPtr OpcUaLWM2MLib::createDataValue(const DeviceDataValue* val)
   return dataValue;
 
 }
-
 
 } /* namespace OpcUalwm2m */
 

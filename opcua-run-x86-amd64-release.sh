06e9e2b305cdd30f8bb98c3be84a36a5aabf7e5a
Manuel Schappacher <manuel.schappacher@hs-offenburg.de>
2018-04-25 08:22:04 +0200

    updated run scripts to the according configurations

--- opcua-run-x86-amd64-release.sh
+++ opcua-run-x86-amd64-release.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 #create the directory for the log files if not existent
-mkdir -p asneg/build/var/log/OpcUaStack
+mkdir -p asneg/build-x86-amd64-release/var/log/OpcUaStack
 
 #copy the according configuration files
-cp -rf cfg/etc asneg/build
+cp -rf cfg/etc asneg/build-x86-amd64-release
 
 #run the OPC UA server
-cd asneg/build
+cd asneg/build-x86-amd64-release
 #./OpcUaServer OpcUaStack etc/OpcUaStack/OpcUaServer.xml
 ./OpcUaServer4 etc/OpcUaStack/OpcUaServer.xml

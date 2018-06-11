9351207daa34d9e1406383b40cf6fee783e079cb
NIKI4.0 <niki40@niki4.0-server>
2018-06-11 15:25:31 +0200

    edited boost_build.sh
    Signed-off-by: NIKI4.0 <niki40@niki4.0-server>

--- niki/gateway/helpers/Dependency Installers/boost-build.sh
+++ niki/gateway/helpers/Dependency Installers/boost-build.sh
@@ -9,53 +9,73 @@
 # cd /usr/bin/
 # sudo ln -s arm-linux-gnueabihf-gcc arm-linux-gnueabihf-gcc-x
 # please set VERSION and BOOST_VERSION
-VERSION=1.54.0
 BOOST=boost_
 BASE_DIR=../../../../../
 
-if [ -z "$1" ]
+add-apt-repository "deb http://dk.archive.ubuntu.com/ubuntu/ xenial main" 
+add-apt-repository "deb http://dk.archive.ubuntu.com/ubuntu/ xenial universe"
+apt-get update
+
+apt-get install cmake
+
+if [ ! -z "$1" ]
 	then
-	BOOST_VERSION=1_54_0
+	if [ $1 = '1_67_0' ]
+		then
+		BOOST_VERSION=1_67_0
+		VERSION=1.67.0
+	fi
 	else 
-	BOOST_VERSION=$1
+	BOOST_VERSION=1_54_0
+	VERSION=1.54.0
 fi
 
-if [ -z "$2" ]
+if [ ! -z "$2" ]
 	then
-	INSTALL_DIR=$BASE_DIR/boost-arm
-	ARM_FLAG=1
+	if [ $2 = 'arm' ]
+		then
+		INSTALL_DIR=$BASE_DIR/boost_$BOOST_VERSION-arm
+		apt-get install gcc-4.9-arm-linux-gnueabihf
+		ARM_FLAG=1
+	fi
 	else 
-	INSTALL_DIR=$BASE_DIR/boost-x86
+	INSTALL_DIR=$BASE_DIR/boost_$BOOST_VERSION-x86
+	apt-get install gcc-4.9 g++-4.9 cpp-4.9
 	ARM_FLAG=0
 fi
 
-if [ ! -e ${BOOST}${BOOST_VERSION}".tar.bz2" ]; then
-    wget -c http://sourceforge.net/projects/boost/files/boost/$VERSION"/"$BOOST_VERSION.tar.bz2
-fi
-
-if [ ! -e ${BOOST}${BOOST_VERSION} ]; then
-    tar -xjvf ${BOOST}${BOOST_VERSION}" .tar.bz2"
-fi
+# Not the best way to do this, will mess up newer gcc versions... 
+ rm /usr/bin/gcc /usr/bin/g++ /usr/bin/cpp
+ ln -s /usr/bin/gcc-4.9 /usr/bin/gcc
+ ln -s /usr/bin/g++-4.9 /usr/bin/g++
+ ln -s /usr/bin/cpp-4.9 /usr/bin/cpp
 
 if [ ! -e "$INSTALL_DIR" ]; then
    mkdir -p $INSTALL_DIR
 fi
 
-if [ ! -e "$INSTALL_DIR/lib/libboost_thread.so" ]; then
-    cd ${BOOST}${BOOST_VERSION}"/"
+if [ ! -e ${INSTALL_DIR}/${BOOST}${BOOST_VERSION}".tar.bz2" ]; then
+    wget -c http://sourceforge.net/projects/boost/files/boost/$VERSION"/"${BOOST}${BOOST_VERSION}.tar.bz2 --directory-prefix=$INSTALL_DIR
+fi
+
+tar -xjvf ${INSTALL_DIR}/${BOOST}${BOOST_VERSION}".tar.bz2" -C ${INSTALL_DIR}
+
+if [ ! -e "$INSTALL_DIR/${BOOST}${BOOST_VERSION}/lib/libboost_thread.so" ]; then
+    echo "starting boost setup"	
+    cd $INSTALL_DIR/${BOOST}${BOOST_VERSION}"/"
+    INSTALL_DIR=$(pwd)		
+    echo "cd to install dir" $INSTALL_DIR 	
     ./bootstrap.sh
-    if [ $ARM_FLAG -eq 1 ]
+    if [ $ARM_FLAG -eq 1 ]${BOOST}${BOOST_VERSION}
 	then
-	sed -i '12s/.*/  using gcc : arm : arm-linux-gnueabihf-g++-4.9 : <compileflags>-std=c++11 ;/' project-config.jam
-    	./b2 install --prefix=$INSTALL_DIR -link=shared toolset=gcc-arm
+	sed -i '12s/.*/  using gcc : arm : arm-linux-gnueabihf-gcc-4.9 : <compileflags>-std=c++11 ;/' project-config.jam
+	echo "Installing with b2 to" $INSTALL_DIR/boost-arm_${BOOST_VERSION}
+    	./b2 install --prefix=$INSTALL_DIR/boost-arm_${BOOST_VERSION} -link=shared toolset=arm-linux-gnueabihf-gcc-4.9
 	else 
-	sed -i '12s/.*/  using gcc : 4.9 : g++-4.9 : <compileflags>-std=c++11 ;/' project-config.jam
- 	./b2 install --prefix=$INSTALL_DIR -link=shared toolset=gcc
+	#sed -i '12s/.*/  using gcc : 4.9 : g++-4.9 : <compileflags>-std=c++11 ;/' project-config.jam
+	echo "Installing with b2 to" $INSTALL_DIR/boost-arm_${BOOST_VERSION}
+ 	./b2 install --prefix=$INSTALL_DIR/boost-x86_${BOOST_VERSION} -link=shared toolset=gcc-4.9
      fi
 fi
-if [ ! -e "$INSTALL_DIR/lib/libboost_thread.so" ]; then
-echo "======================================="
-echo "$INSTALL_DIR/lib/libboost_thread.so not succefully build ... aborting"
-echo "remove the cause of the error and start again"
-exit 1
-fi
+
+rm ${INSTALL_DIR}/${BOOST}${BOOST_VERSION}".tar.bz2" 

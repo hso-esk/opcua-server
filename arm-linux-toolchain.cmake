cbe018d7b490458f87ff1cf827b61147175295ca
Dovydas Girdvainis <dovydas.girdvainis@gmail.com>
2018-06-14 10:00:38 +0200

    modified toolchain files to conform with dependency installers, added a fix for asneg certificate problem
    Signed-off-by: Dovydas Girdvainis <dovydas.girdvainis@gmail.com>

--- arm-linux-toolchain.cmake
+++ arm-linux-toolchain.cmake
@@ -22,11 +22,11 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET(NO_CMAKE_SYSTEM_PATH    ON)
 
 #set boost library path
-SET(BOOST_ROOT              "${CMAKE_SOURCE_DIR}/../../../../boost-arm")
+SET(BOOST_ROOT              "${CMAKE_SOURCE_DIR}/../../../../boost-arm_1_54_0")
 SET(Boost_NO_SYSTEM_PATHS   ON)
 
 #set OpenSSL Library path
-SET(OPENSSL_ROOT_DIR        "${CMAKE_SOURCE_DIR}/../../../../openssl-arm")
+SET(OPENSSL_ROOT_DIR        "${CMAKE_SOURCE_DIR}/../../../../openssl")
 
 #set linker flags for ODBC
 SET(ENV{LDFLAGS} "$ENV{LDFLAGS} -L${CMAKE_SOURCE_DIR}/../../../../odbc-arm/lib")
--- x86-amd64-linux-toolchain.cmake
+++ x86-amd64-linux-toolchain.cmake
@@ -22,5 +22,8 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET(NO_CMAKE_SYSTEM_PATH    ON)
 
 #set boost library path
-SET(BOOST_ROOT              "${CMAKE_SOURCE_DIR}/../../../../boost-x86")
+SET(BOOST_ROOT              "${CMAKE_SOURCE_DIR}/../../../../boost-x86_1_54_0")
 SET(Boost_NO_SYSTEM_PATHS   ON)
+
+#set linker flags for ODBC
+SET(ENV{LDFLAGS} "$ENV{LDFLAGS} -L${CMAKE_SOURCE_DIR}/../../../../odbc-x86/lib")
